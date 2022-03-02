37ad7fc00bd5207b925db8f6384f067abe32e041
ijbrave <ssjavad00@gmail.com>
2022-03-02 12:43:19 +0330

    feat(goal): add protect to APIs

--- backend/routes/goal.ts
+++ backend/routes/goal.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { createGoal, deleteGoal, getGoals, updateGoal } from '../controllers'
+import { protect } from '../middleware'
 
 const router = Router()
 
-router.route('/').get(getGoals).post(createGoal)
+router.route('/').get(protect, getGoals).post(protect,createGoal)
 
-router.route('/:goalId').put(updateGoal).delete(deleteGoal)
+router.route('/:goalId').put(protect, updateGoal).delete(protect,deleteGoal)
 
 export default router
