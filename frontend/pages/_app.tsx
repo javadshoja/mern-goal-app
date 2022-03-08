ccb7f1bba33546726131d8412f70addf2e5e6b00
ijbrave <ssjavad00@gmail.com>
2022-03-08 19:13:06 +0330

    feat(_app): add Layout

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import { Layout } from '../components'
+
 import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
 
@@ -7,7 +9,9 @@ import store from '../app/store'
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
-			<Component {...pageProps} />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
 		</Provider>
 	)
 }
