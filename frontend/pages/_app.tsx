404cf9cd172b2aefe469d84dfe5ce65da50f134c
ijbrave <ssjavad00@gmail.com>
2022-04-08 18:26:21 +0430

    Add ToastContainer

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
 import { Layout } from '../components'
-
 import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import store from '../app/store'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -11,6 +11,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 		<Provider store={store}>
 			<Layout>
 				<Component {...pageProps} />
+				<ToastContainer position='top-center' />
 			</Layout>
 		</Provider>
 	)
