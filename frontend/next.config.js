0ddf588e81f4c8ab037cbde1a87f19a2ecef7a9f
ijbrave <ssjavad00@gmail.com>
2022-04-08 17:41:29 +0430

    Add redirect api to server endpoint

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 
+const process = require('process')
+
 const nextConfig = {
 	reactStrictMode: true,
 	compiler: {
-		styledComponents: true,
+		styledComponents: true
 	},
+	async redirects() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `${
+					process.env.SERVER_ENDPOINT
+						? process.env.SERVER_ENDPOINT
+						: 'http:/localhost:4000'
+				}/api/:path*`,
+				permanent: true
+			}
+		]
+	}
 }
 
 module.exports = nextConfig
