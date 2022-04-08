056c7a94b16eef65621f9502199afdc17bdf05f4
ijbrave <ssjavad00@gmail.com>
2022-04-08 18:35:31 +0430

    feat(components): add logout to Navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,28 +1,57 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { HiOutlineLogin, HiOutlineLogout, HiOutlineUser } from 'react-icons/hi'
-import { Nav, Logo, Items, Item, NavLink } from './styles'
+import { useSelector, useDispatch } from 'react-redux'
+import styled from 'styled-components'
+import { RootState } from '../app/store'
+import { logout, reset } from '../features/user/userSlice'
+import { Nav, Logo, Items, Item, NavLink, Button } from './styles'
 
 function Header() {
+	const router = useRouter()
+
+	const dispatch = useDispatch()
+
+	const user = useSelector((state: RootState) => state.user.user)
+
+	const OnLogout = () => {
+		dispatch(logout())
+		dispatch(reset())
+		router.push('/')
+	}
+
 	return (
 		<Nav>
 			<Logo>
 				<Link href='/'>GoalSetter 🎯</Link>
 			</Logo>
 			<Items>
-				<Item>
-					<Link href='/login'>
-						<NavLink>
-							<HiOutlineLogin /> Login
-						</NavLink>
-					</Link>
-				</Item>
-				<Item>
-					<Link href='/register'>
-						<NavLink>
-							<HiOutlineUser /> Register
-						</NavLink>
-					</Link>
-				</Item>
+				{user ? (
+					<>
+						<Item>
+							<Button onClick={OnLogout}>
+								<HiOutlineLogout /> Logout
+							</Button>
+						</Item>
+					</>
+				) : (
+					<>
+						<Item>
+							<Link href='/login'>
+								<NavLink>
+									<HiOutlineLogin /> Login
+								</NavLink>
+							</Link>
+						</Item>
+						<Item>
+							<Link href='/register'>
+								<NavLink>
+									<HiOutlineUser /> Register
+								</NavLink>
+							</Link>
+						</Item>
+					</>
+				)}
 			</Items>
 		</Nav>
 	)
