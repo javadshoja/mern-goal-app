b78c9003e014b7c41f1953085579af96670ed034
ijbrave <ssjavad00@gmail.com>
2022-04-08 18:30:16 +0430

    feat(app): add userReducer to store

--- frontend/app/store.ts
+++ frontend/app/store.ts
@@ -1,11 +1,15 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import userReducer from '../features/user/userSlice'
 
 export function makeStore() {
 	return configureStore({
-		reducer: {}
+		reducer: { user: userReducer }
 	})
 }
 
 const store = makeStore()
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 
 export default store
