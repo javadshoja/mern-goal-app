09ea5487d5cb18de7d534df0b4309c127a5f0d18
ijbrave <ssjavad00@gmail.com>
2022-04-23 13:18:20 +0430

    feat(styles): add Heading

---
+++ frontend/components/styles/Heading.styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Heading = styled.section`
+	font-size: 2rem;
+	font-weight: 700;
+	margin-bottom: 50px;
+	padding: 0 20px;
+
+	p {
+		color: #5f79ce;
+	}
+`
+export default Heading
