66aa6001a22693ec76a56657646efab38b20083f
ijbrave <ssjavad00@gmail.com>
2022-04-23 13:12:53 +0430

    fix(styles): change Container name

--- frontend/components/styles/Container.styled.ts
+++ frontend/components/styles/Container.styled.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
-const Container = styled.div`
+const HeroContainer = styled.div`
 	width: 100%;
 	max-width: 960px;
 	margin: 0 auto;
 	padding: 0 20px;
 	text-align: center;
 `
-export default Container
+export default HeroContainer
